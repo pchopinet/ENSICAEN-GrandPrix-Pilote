b617c10ffdbaed11af25ed6689b85b54b0558a93
pchopinet <pierre.chopinet97@gmail.com>
2018-04-10 11:11:07 +0200

    add pilote struct

---
+++ include/pilote/pilote.h
@@ -0,0 +1,17 @@
+//
+// Created by chopinet on 10/04/18.
+//
+
+#ifndef ENSICAEN_GRANDPRIX_PILOTE_PILOTE_H
+#define ENSICAEN_GRANDPRIX_PILOTE_PILOTE_H
+
+typedef struct pilote_t {
+    int x;
+    int y;
+    int vx;
+    int vy;
+}Pilote;
+
+
+
+#endif //ENSICAEN_GRANDPRIX_PILOTE_PILOTE_H
---
+++ src/pilote/pilote.c
@@ -0,0 +1,7 @@
+//
+// Created by chopinet on 10/04/18.
+//
+
+
+#include "../../include/pilote/pilote.h"
+
