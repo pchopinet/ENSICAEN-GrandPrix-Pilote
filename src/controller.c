557fec6e3f188adfde13318465ddc773a2419b59
pchopinet <pierre.chopinet97@gmail.com>
2018-04-29 12:14:38 +0200

    add readposition

--- src/controller.c
+++ src/controller.c
@@ -4,14 +4,30 @@
 
 #include "../include/controller.h"
 #include <math.h>
+#include "../lib/CCollections/include/util.h"
 
+/**
+ *
+ * @warning NOT TESTED
+ * @return Arraylist with the position of cars
+ */
 ArrayList readPosition() {
-    ArrayList position = newArrayList(3, 1);
+    ArrayList position = newArrayList(3, 1, sizeof(struct point_t));
 
     char c;
+    int x = 0, y = 0, i = 0;
     while (fread(&c, sizeof(char), 1, stdin) == 1 && c != '\n') {
-
+        if (c < '9' && c > '0') {
+            if (i % 2 == 0) {
+                x = c - '0';
+            } else {
+                y = c - '0';
+                ArrayListAppend(position, newPoint(x, y));
+            }
+            i++;
+        }
     }
+    return position;
 
 }
 
@@ -28,14 +44,14 @@ ArrayList getPointAccessible(Track t, Point position, Vector speed) {
     int x = PointX(position);
     int y = PointY(position);
 
-    ArrayList accessible = newArrayList(4, 1);
+    ArrayList accessible = newArrayList(4, 1, sizeof(struct point_t));
     Point p1 = newPoint(x + 1, y);
     Point p2 = newPoint(x - 1, y);
     Point p3 = newPoint(x, y + 1);
     Point p4 = newPoint(x, y - 1);
 
     if (isAccessible(t, p1))
-        ArrayListAppend(accessible, p1);
+        ArrayListAppend(accessible, (T) p1);
     if (isAccessible(t, p2))
         ArrayListAppend(accessible, p2);
     if (isAccessible(t, p3))
@@ -46,7 +62,7 @@ ArrayList getPointAccessible(Track t, Point position, Vector speed) {
     return accessible;
 }
 
-void Dijkstra(Track t, Point p, Vector speed) {
+Point *Dijkstra(Track t, Point p, Vector speed) {
 
 }
 
