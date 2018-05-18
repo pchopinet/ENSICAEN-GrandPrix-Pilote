7f93863608a51a9cb595e148b4571051c4e6c366
pchopinet <pierre.chopinet97@gmail.com>
2018-05-18 09:32:26 +0200

    create function haveneoughfuel

--- src/anakinFull.c
+++ src/anakinFull.c
@@ -10,18 +10,24 @@
 
 int deltaCarburantAcceleration(int accX, int accY, int velX, int velY, int sable) {
     int valeur = accX * accX + accY * accY;
-    valeur += (int)(sqrt(velX * velX + velY * velY) * 3.0 / 2.0);
+    valeur += (int) (sqrt(velX * velX + velY * velY) * 3.0 / 2.0);
     valeur += sable;
     return -valeur;
 }
 
-void sendAcceleration(int ax, int ay, int fuel, FILE* log) {
+void sendAcceleration(int ax, int ay, int fuel, FILE *log) {
     char action[10];
     sprintf(action, "%d %d", ax, ay);
     fprintf(stdout, "%s\n", action);
     fprintf(log, "%s (carburant restant %d)\n", action, fuel);
     fflush(stdout);
     fflush(log);
+}
+
+int haveEnoughFuel(int fuelMax, float totalfuelConso) {
+    int lowerFuel;
+    lowerFuel = (totalfuelConso < 0.95f * (float) fuelMax);
+    return lowerFuel;
 }
 
 int main() {
@@ -38,7 +44,8 @@ int main() {
     float x;
     int dist;
 
-    int lowerFuel;
+    int haveenoughFuel;
+    float totalfuelConso;
 
     FILE *log = fopen("anakinFull.log", "w");
 
@@ -51,47 +58,48 @@ int main() {
     fprintf(log, "\n === Tour %d === \n", tour);
 
 
-    sf = fscanf(stdin,"%d %d\t%d %d\t%d %d",&(a1.y),&(a1.x),&(b.y),&(b.x),&(c.y),&(c.x));
-    fprintf(log,"%d %d\t%d %d\t%d %d\n",a1.y,a1.x,b.y,b.x,c.y,c.x);
+    sf = fscanf(stdin, "%d %d\t%d %d\t%d %d", &(a1.y), &(a1.x), &(b.y), &(b.x), &(c.y), &(c.x));
+    fprintf(log, "%d %d\t%d %d\t%d %d\n", a1.y, a1.x, b.y, b.x, c.y, c.x);
     fflush(log);
 
-    a1.vx=0;
-    a1.vy=0;
+    a1.vx = 0;
+    a1.vy = 0;
 
     x = 10;
 
     L = initLadj(T);
     loadLadj(L, T, a1);
     calculDistance(L);
-    finalPoint = dijkstra(L,T,a1,x);
+    finalPoint = dijkstra(L, T, a1, x);
     route = findRoute(L, finalPoint);
-    lowerFuel = ((float)*totFuel(L,finalPoint) < 0.95f * (float)fuel) ? 0 : 1;
+    totalfuelConso = (float) *totFuel(L, finalPoint);
+    haveenoughFuel = haveEnoughFuel(fuel, totalfuelConso);
 
     a1 = pushStack(route);
     a2 = pushStack(route);
-    ay = a2.vx-a1.vx;
-    ax = a2.vy-a1.vy;
+    ay = a2.vx - a1.vx;
+    ax = a2.vy - a1.vy;
     vy = a1.vx;
     vx = a1.vy;
 
-    fuel += deltaCarburantAcceleration(ax, ay, vx, vy, testPt(T,a1,'~'));
+    fuel += deltaCarburantAcceleration(ax, ay, vx, vy, testPt(T, a1, '~'));
 
-    sendAcceleration(ax,ay,fuel,log);
+    sendAcceleration(ax, ay, fuel, log);
 
     while (!feof(stdin)) {
         tour++;
         fprintf(log, "\nTour: %d\n", tour);
 
-        sf = fscanf(stdin,"%d %d\t%d %d\t%d %d",&(a1.y),&(a1.x),&(b.y),&(b.x),&(c.y),&(c.x));
-        fprintf(log,"%d %d\t%d %d\t%d %d\n",a1.y,a1.x,b.y,b.x,c.y,c.x);
+        sf = fscanf(stdin, "%d %d\t%d %d\t%d %d", &(a1.y), &(a1.x), &(b.y), &(b.x), &(c.y), &(c.x));
+        fprintf(log, "%d %d\t%d %d\t%d %d\n", a1.y, a1.x, b.y, b.x, c.y, c.x);
         fflush(log);
 
 
-        if (a1.x!=a2.x || a1.y!=a2.y) {// Pour repartir en cas de crash
+        if (a1.x != a2.x || a1.y != a2.y) {// Pour repartir en cas de crash
 
-            if (lowerFuel) {
-                x = 0.5*x;
-                fprintf(log,"lowerFuel x = %.0f\n",x);
+            if (!haveenoughFuel) {
+                x = 0.5f * x;
+                fprintf(log, "notEnoughFuel x = %.0f\n", x);
                 fflush(log);
             }
 
@@ -100,14 +108,15 @@ int main() {
             T->track[b.x][b.y] = '.';
             T->track[c.x][c.y] = '.';
 
-            a1.vx=0;
-            a1.vy=0;
+            a1.vx = 0;
+            a1.vy = 0;
 
             L = initLadj(T);
             loadLadj(L, T, a1);
-            finalPoint = dijkstra(L,T,a1,x);
+            finalPoint = dijkstra(L, T, a1, x);
             route = findRoute(L, finalPoint);
-            lowerFuel = ((float)*totFuel(L,finalPoint) < 0.95f * (float)fuel) ? 0 : 1;
+            totalfuelConso = (float) *totFuel(L, finalPoint);
+            haveenoughFuel = haveEnoughFuel(fuel, totalfuelConso);
 
             a1 = pushStack(route);
 
@@ -124,11 +133,12 @@ int main() {
 
         a2 = pushStack(route);
 
-        if ((a2.x==b.x && a2.y==b.y) || (a2.x==c.x && a2.y==c.y) || lowerFuel) {// pour ne pas se crash dans une autre voiture
+        if ((a2.x == b.x && a2.y == b.y) || (a2.x == c.x && a2.y == c.y) ||
+            !haveenoughFuel) {// pour ne pas se crash dans une autre voiture
 
-            if (lowerFuel) {
-                x = 0.5 * x;
-                fprintf(log, "lowerFuel x = %.0f\n", x);
+            if (!haveenoughFuel) {
+                x = 0.5f * x;
+                fprintf(log, "notEnoughFuel x = %.0f\n", x);
                 fflush(log);
             }
 
@@ -141,7 +151,9 @@ int main() {
             loadLadj(L, T, a1);
             finalPoint = dijkstra(L, T, a1, x);
             route = findRoute(L, finalPoint);
-            lowerFuel = ((float)*totFuel(L,finalPoint) < 0.95f * (float)fuel) ? 0 : 1;
+            totalfuelConso = (float) *totFuel(L, finalPoint);
+
+            haveenoughFuel = haveEnoughFuel(fuel, totalfuelConso);
 
             T->track[b.x][b.y] = vb;
             T->track[c.x][c.y] = vc;
@@ -152,13 +164,13 @@ int main() {
 
         }
 
-        ay = a2.vx-a1.vx;
-        ax = a2.vy-a1.vy;
+        ay = a2.vx - a1.vx;
+        ax = a2.vy - a1.vy;
         vy = a1.vx;
         vx = a1.vy;
 
-        fuel += deltaCarburantAcceleration(ax, ay, vx, vy, testPt(T,a1,'~'));
-        sendAcceleration(ax,ay,fuel,log);
+        fuel += deltaCarburantAcceleration(ax, ay, vx, vy, testPt(T, a1, '~'));
+        sendAcceleration(ax, ay, fuel, log);
     }
     return sf;
 }
