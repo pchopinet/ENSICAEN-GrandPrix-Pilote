949618e45d2ebbf3c344eeb59fa70a93a7482a49
pchopinet <pierre.chopinet97@gmail.com>
2018-05-21 11:31:44 +0200

    add some free

--- src/graph.c
+++ src/graph.c
@@ -1,6 +1,6 @@
 #include "../include/graph.h"
 
-Ladj* initLadj(Track t) {
+Ladj *initLadj(Track t) {
 
     point p;
     Ladj *L = malloc(sizeof(Ladj));
@@ -32,14 +32,14 @@ Ladj* initLadj(Track t) {
         L->start[p.x].vy = 0;
     }
 
-    L->node = (Lnode*****) malloc(t->height*sizeof(Lnode****));
-    for (p.x=0; p.x<t->height; p.x++) {
-        L->node[p.x] = (Lnode****) malloc(t->width*sizeof(Lnode***));
-        for (p.y=0; p.y<t->width; p.y++) {
-            L->node[p.x][p.y] = (Lnode***) malloc(11*sizeof(Lnode**));
-            for (p.vx=0; p.vx<11; p.vx++) {
-                L->node[p.x][p.y][p.vx] = (Lnode**) calloc(11,sizeof(Lnode*));
-                for (p.vy=0; p.vy<11; p.vy++) {
+    L->node = (Lnode *****) malloc(t->height * sizeof(Lnode ****));
+    for (p.x = 0; p.x < t->height; p.x++) {
+        L->node[p.x] = (Lnode ****) malloc(t->width * sizeof(Lnode ***));
+        for (p.y = 0; p.y < t->width; p.y++) {
+            L->node[p.x][p.y] = (Lnode ***) malloc(11 * sizeof(Lnode **));
+            for (p.vx = 0; p.vx < 11; p.vx++) {
+                L->node[p.x][p.y][p.vx] = (Lnode **) calloc(11, sizeof(Lnode *));
+                for (p.vy = 0; p.vy < 11; p.vy++) {
                     L->node[p.x][p.y][p.vx][p.vy] = createLnode();
                 }
             }
@@ -48,66 +48,95 @@ Ladj* initLadj(Track t) {
     return L;
 }
 
-void freeLadj(Ladj* L, Track t) {
+void freeLadj(Ladj *L, Track t) {
     point p;
-    Cell* C;
-    for (p.x=0; p.x<t->height; p.x++) {
-        for (p.y=0; p.y<t->width; p.y++) {
-            for (p.vx=0; p.vx<11; p.vx++) {
-                for (p.vy=0; p.vy<11; p.vy++) {
-                    free(L->node[p.x][p.y][p.vx][p.vy]->next);
-                        //for (C=L->node[p.x][p.y][p.vx][p.vy]->next, C=C->next,)
-                    free(L->node[p.x][p.y][p.vx][p.vy]->dijNext);
-                    free(L->node[p.x][p.y][p.vx][p.vy]->prev);
-                    free(L->node[p.x][p.y][p.vx][p.vy]->dijPrev);
-                    free(L->node[p.x][p.y][p.vx][p.vy]);
+    Cell *C;
+    Cell *temp;
+    for (p.x = 0; p.x < t->height; p.x++) {
+        for (p.y = 0; p.y < t->width; p.y++) {
+            for (p.vx = 0; p.vx < 11; p.vx++) {
+                for (p.vy = 0; p.vy < 11; p.vy++) {
+                    //free(L->node[p.x][p.y][p.vx][p.vy]->next);
+                    C = L->node[p.x][p.y][p.vx][p.vy]->next;
+
+                    while (C != NULL) {
+                        temp = C;
+                        C = C->next;
+                        free(temp);
+                    }
+                    //free(L->node[p.x][p.y][p.vx][p.vy]);
+
+                    /*C = L->node[p.x][p.y][p.vx][p.vy]->dijNext;
+                    while (C!=NULL){
+                        temp = C;
+                        C = C->next;
+                        free(temp);
+                    }
+
+                    C = L->node[p.x][p.y][p.vx][p.vy]->dijPrev;
+                    while (C!=NULL){
+                        temp = C;
+                        C = C->next;
+                        free(temp);
+                    }
+
+                    C = L->node[p.x][p.y][p.vx][p.vy]->prev;
+                    while (C!=NULL){
+                        temp = C;
+                        C = C->next;
+                        free(temp);
+                    }*/
+                    //free(L->node[p.x][p.y][p.vx][p.vy]->dijNext);
+                    //free(L->node[p.x][p.y][p.vx][p.vy]->prev);
+                    //free(L->node[p.x][p.y][p.vx][p.vy]->dijPrev);
+                    //free(L->node[p.x][p.y][p.vx][p.vy]);
 
                 }
-                free(L->node[p.x][p.y][p.vx]);
+                //free(L->node[p.x][p.y][p.vx]);
             }
             free(L->node[p.x][p.y]);
         }
         free(L->node[p.x]);
     }
+    free(L->node);
+    free(L);
 }
 
 int loadLadj(Ladj *L, Track T, point p) {
 
     int normSpeed2, ax, ay, fuel;
-    int f=0;
+    int f = 0;
     point t, h;
-    Queue* Q = createQueue();
+    Queue *Q = createQueue();
     put(p, Q);
 
 
     while (!isEmpty(Q)) {
 
-        t=push(Q);
+        t = push(Q);
 
-        *tag(L,t)=2;
+        *tag(L, t) = 2;
         L->nbNode++;
 
 
-
-        for (ax=-1; ax<2; ax++) {
-            for (ay=-1; ay<2; ay++) {
+        for (ax = -1; ax < 2; ax++) {
+            for (ay = -1; ay < 2; ay++) {
 
                 h.vx = t.vx + ax;
                 h.vy = t.vy + ay;
                 h.x = t.x + h.vx;
                 h.y = t.y + h.vy;
 
-                normSpeed2 = h.vx*h.vx + h.vy*h.vy;
+                normSpeed2 = h.vx * h.vx + h.vy * h.vy;
                 //fuel = ax*ax+ay*ay + (int) (sqrt(normSpeed2)*3/2);
-                fuel = ax*ax+ay*ay + (int) (sqrt(h.vx*h.vx + h.vy*h.vy)*3/2);
-                fuel += testPt(T,t,'~');
+                fuel = ax * ax + ay * ay + (int) (sqrt(h.vx * h.vx + h.vy * h.vy) * 3 / 2);
+                fuel += testPt(T, t, '~');
 
 
+                if (pointInTrack(h, L) && normSpeed2 <= 25 && reachable2(T, t, h) &&
+                    (!testPt(T, t, '~') || normSpeed2 <= 1)) {
 
-                if (pointInTrack(h, L) && normSpeed2<=25 && reachable2(T, t, h) &&
-                    (!testPt(T,t,'~') || normSpeed2<=1)) {
-
-                    if ((testPt(T,h,'#') || testPt(T,h,'~')) && *tag(L, h) != 2) {
+                    if ((testPt(T, h, '#') || testPt(T, h, '~')) && *tag(L, h) != 2) {
 
                         if (*tag(L, h) == 0) {
                             put(h, Q);
@@ -115,9 +144,9 @@ int loadLadj(Ladj *L, Track T, point p) {
                         }
                         newArc(h, t, fuel, ax, ay, L);
 
-                    } else if (testPt(T,h,'=')) {
+                    } else if (testPt(T, h, '=')) {
                         newArc(h, t, fuel, ax, ay, L);
-                        L->finish[f]=h;
+                        L->finish[f] = h;
                         f++;
                     }
                 } else {
@@ -140,8 +169,8 @@ int loadLadj(Ladj *L, Track T, point p) {
         }
     }
 
-    L->nbFinish=f;
-    if (f==0) {
+    L->nbFinish = f;
+    if (f == 0) {
         printf("Erreur arrivée non atteinte!\n");
         return -1;
     } else {
@@ -149,28 +178,28 @@ int loadLadj(Ladj *L, Track T, point p) {
     }
 }
 
-int calculDistance(Ladj* L){
+int calculDistance(Ladj *L) {
 
-    Queue* Q = createQueue();
-    Cell* C;
+    Queue *Q = createQueue();
+    Cell *C;
     point p, q;
     int i;
-    int d=0;
+    int d = 0;
 
-    for (i=0; i<L->nbFinish; i++) {
-        put(L->finish[i],Q);
-        *distance(L,L->finish[i]) = 0;
+    for (i = 0; i < L->nbFinish; i++) {
+        put(L->finish[i], Q);
+        *distance(L, L->finish[i]) = 0;
     }
 
     while (!isEmpty(Q)) {
         p = push(Q);
-        d = *distance(L,p);
-        C = *prev(L,p);
-        while (C!=NULL) {
+        d = *distance(L, p);
+        C = *prev(L, p);
+        while (C != NULL) {
             q = C->head;
-            if (*distance(L,q) == -1) {
-                *distance(L,q) = d+1;
-                put(q,Q);
+            if (*distance(L, q) == -1) {
+                *distance(L, q) = d + 1;
+                put(q, Q);
             }
             C = C->next;
 
@@ -180,65 +209,65 @@ int calculDistance(Ladj* L){
 }
 
 
-Stack* findRoute(Ladj* L, point p) {
+Stack *findRoute(Ladj *L, point p) {
 
-    Stack* S = createStack();
-    Cell* C;
+    Stack *S = createStack();
+    Cell *C;
 
     do {
-        putStack(p,S);
-        C = *dijPrev(L,p);
+        putStack(p, S);
+        C = *dijPrev(L, p);
         p = C->head;
-    } while (*totFuel(L,p) != 0);
+    } while (*totFuel(L, p) != 0);
 
-    putStack(p,S);
+    putStack(p, S);
     return S;
 }
 
-point dijkstra(Ladj* L, Track t, point a, float x) {
+point dijkstra(Ladj *L, Track t, point a, float x) {
     int w;
     int *TFa, *TFb;
     float *TWa, *TWb;
     point b;
-    Cell* C;
-    List* list = createList();
+    Cell *C;
+    List *list = createList();
 
     putInList(list, a, 0);
-    *totFuel(L,a) = 0;
-    *totWeight(L,a) = 0;
-    while (!testPt(t,a,'=')) {
+    *totFuel(L, a) = 0;
+    *totWeight(L, a) = 0;
+    while (!testPt(t, a, '=')) {
 
         a = getMin(list);
-        *tag(L,a)=4;
+        *tag(L, a) = 4;
 
 
-        C = *next(L,a);
+        C = *next(L, a);
         TFa = totFuel(L, a);
         TWa = totWeight(L, a);
 
-        while (C!=NULL) {
+        while (C != NULL) {
             b = C->head;
             TFb = totFuel(L, b);
             TWb = totWeight(L, b);
 
-            if (*tag(L,b)!=4) {
+            if (*tag(L, b) != 4) {
 
-                w = 1000*C->fuel + (int)(1000*x);
+                w = 1000 * C->fuel + (int) (1000 * x);
 
-                if ((*totWeight(L,a) + w) < *totWeight(L,b)) {
+                if ((*totWeight(L, a) + w) < *totWeight(L, b)) {
 
                     *TFb = *TFa + C->fuel;
                     *TWb = *TWa + w;
 
                     if (*tag(L, b) < 3) {
 
-                        *tag(L,b)=3;
-                        putInList(list,b,*TWb);
+                        *tag(L, b) = 3;
+                        putInList(list, b, *TWb);
                         newArcDij(b, a, w, C->ax, C->ay, L);
 
-                    } else if (*tag(L, b) == 3){
+                    } else if (*tag(L, b) == 3) {
 
-                        changeTotWeight(list,b,*TWb);
+                        changeTotWeight(list, b, *TWb);
                         newArcDij(b, a, w, C->ax, C->ay, L);
                     }
                 }
