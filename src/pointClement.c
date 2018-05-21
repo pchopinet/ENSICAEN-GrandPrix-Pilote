44e84ea0b257cd8309d97c547daf4088a6228abc
pchopinet <pierre.chopinet97@gmail.com>
2018-05-21 11:03:10 +0200

    fix double px==qx

--- src/pointClement.c
+++ src/pointClement.c
@@ -1,5 +1,5 @@
 #include "../include/pointClement.h"
 
 int ptEqual(point p, point q) {
-    return p.x==q.x && p.x==q.x && p.y==q.y && p.vx==q.vx && p.vy==q.vy;
+    return p.x == q.x && p.y == q.y && p.vx == q.vx && p.vy == q.vy;
 }
