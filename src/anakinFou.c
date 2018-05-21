d8ccf9f1a0bb0c42d8265afcdd6de0d85ad858e3
pchopinet <pierre.chopinet97@gmail.com>
2018-05-21 11:43:56 +0200

    fix tour == 1 for free

--- src/anakinFou.c
+++ src/anakinFou.c
@@ -10,12 +10,12 @@
 
 int deltaCarburantAcceleration(int accX, int accY, int velX, int velY, int sable) {
     int valeur = accX * accX + accY * accY;
-    valeur += (int)(sqrt(velX * velX + velY * velY) * 3.0 / 2.0);
+    valeur += (int) (sqrt(velX * velX + velY * velY) * 3.0 / 2.0);
     valeur += sable;
     return -valeur;
 }
 
-void sendAcceleration(int ax, int ay, int fuel, FILE* log) {
+void sendAcceleration(int ax, int ay, int fuel, FILE *log) {
     char action[10];
     sprintf(action, "%d %d", ax, ay);
     fprintf(stdout, "%s\n", action);
@@ -36,7 +36,7 @@ int main() {
     Ladj *L;
 
     FILE *log = fopen("anakinFou.log", "w");
-
+    //log = stdout;
     Track T = initTrack(stdin);
     TrackPrint(T, log);
     fuel = T->fuel;
@@ -45,13 +45,13 @@ int main() {
 
         tour++;
         fprintf(log, "\nTour: %d\n", tour);
-        sf = fscanf(stdin,"%d %d\t%d %d\t%d %d",&(a1.y),&(a1.x),&(b.y),&(b.x),&(c.y),&(c.x));
-        fprintf(log,"\tv1: %d %d\tv2: %d %d\tv3: %d %d\n",a1.y,a1.x,b.y,b.x,c.y,c.x);
+        sf = fscanf(stdin, "%d %d\t%d %d\t%d %d", &(a1.y), &(a1.x), &(b.y), &(b.x), &(c.y), &(c.x));
+        fprintf(log, "\tv1: %d %d\tv2: %d %d\tv3: %d %d\n", a1.y, a1.x, b.y, b.x, c.y, c.x);
         fflush(log);
 
-        if (tour == 1 || a1.x!=a2.x || a1.y!=a2.y) {
+        if (tour == 1 || a1.x != a2.x || a1.y != a2.y) {
             crash = 1;
-            fprintf(log,"\tCRASH\n");
+            fprintf(log, "\tCRASH\n");
             fflush(log);
         } else {
             a1 = a2;
@@ -59,21 +59,21 @@ int main() {
             crash = 0;
         }
 
-        if (crash == 1 || (a2.x==b.x && a2.y==b.y) || (a2.x==c.x && a2.y==c.y) || lowerFuel || increaseFuel) {
+        if (crash == 1 || (a2.x == b.x && a2.y == b.y) || (a2.x == c.x && a2.y == c.y) || lowerFuel || increaseFuel) {
 
-            fprintf(log,"\tNouvelle trajectoire : ");
+            fprintf(log, "\tNouvelle trajectoire : ");
             if (lowerFuel) {
                 x1 = x;
-                x = (x0+x1)/2;
-                fprintf(log," - lowFuel x = %.2f.\n", x);
+                x = (x0 + x1) / 2;
+                fprintf(log, " - lowFuel x = %.2f.\n", x);
             } else if (increaseFuel) {
                 x0 = x;
-                x = (x0+x1)/2;
-                fprintf(log," + incFuel x = %.2f.\n",x);
+                x = (x0 + x1) / 2;
+                fprintf(log, " + incFuel x = %.2f.\n", x);
             } else {
                 x0 = 0;
-                x = (x0+x1)/2;
-                fprintf(log," route bloquée.\n");
+                x = (x0 + x1) / 2;
+                fprintf(log, " route bloquée.\n");
             }
             fflush(log);
 
@@ -86,15 +86,16 @@ int main() {
                 a1.vx = 0;
                 a1.vy = 0;
             }
-            if (tour==1) {
-                //freeLadj(L,T);
+            if (tour > 1) {
+                fprintf(log, "Free");
+                freeLadj(L, T);
             }
             L = initLadj(T);
             loadLadj(L, T, a1);
-            finalPoint = dijkstra(L,T,a1,x);
+            finalPoint = dijkstra(L, T, a1, x);
             route = findRoute(L, finalPoint);
-            lowerFuel = ((float)*totFuel(L,finalPoint) < 0.95f * (float)fuel) ? 0 : 1;
-            increaseFuel = ((float)*totFuel(L,finalPoint) > 0.90f * (float)fuel) ? 0 : 1;
+            lowerFuel = ((float) *totFuel(L, finalPoint) < 0.95f * (float) fuel) ? 0 : 1;
+            increaseFuel = ((float) *totFuel(L, finalPoint) > 0.90f * (float) fuel) ? 0 : 1;
 
             a1 = pushStack(route);
             a2 = pushStack(route);
@@ -103,11 +104,11 @@ int main() {
             T->track[c.x][c.y] = vc;
         }
 
-        ay = a2.vx-a1.vx;
-        ax = a2.vy-a1.vy;
+        ay = a2.vx - a1.vx;
+        ax = a2.vy - a1.vy;
 
-        fuel += deltaCarburantAcceleration(ax, ay, a1.vx, a1.vy, testPt(T,a1,'~'));
-        sendAcceleration(ax,ay,fuel,log);
+        fuel += deltaCarburantAcceleration(ax, ay, a1.vx, a1.vy, testPt(T, a1, '~'));
+        sendAcceleration(ax, ay, fuel, log);
 
     } while (!feof(stdin));
     return sf;
